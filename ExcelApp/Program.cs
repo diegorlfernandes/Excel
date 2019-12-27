47d5eefb85efad4f99b96c643c8e770f153d53cc
Diego Rodrigues de Lima Fernandes <diego.fernandes@ms.senac.br>
2019-12-27 13:25:06 -0400

    commit inicial

---
+++ ExcelApp/Program.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LerOLEDB();
+            Console.ReadKey();
+        }
+
+        static void criar()
+        {
+            // Inicia o componente Excel
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+            //Cria uma planilha temporária na memória do computador
+            xlApp = new Excel.Application();
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            //incluindo dados
+            xlWorkSheet.Cells[1, 1] = "Dados do cliente:";
+            xlWorkSheet.Cells[1, 2] = "Nome";
+            xlWorkSheet.Cells[1, 3] = "Endereço";
+            xlWorkSheet.Cells[1, 4] = "CPF";
+
+            //Salva o arquivo de acordo com a documentação do Excel.
+            xlWorkBook.SaveAs("arquivo.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue,
+            Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            //o arquivo foi salvo na pasta Meus Documentos.
+            string caminho = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            Console.WriteLine("Concluído. Verifique em " + caminho + "arquivo.xls");
+
+        }
+
+        static void ler()
+        {
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"c:\\tmp\\arquivo.xls");
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            int rowCount = xlRange.Rows.Count;
+            int colCount = xlRange.Columns.Count;
+
+            //iterate over the rows and columns and print to the console as it appears in the file
+            //excel is not zero based!!
+            for (int i = 1; i <= rowCount; i++)
+            {
+                for (int j = 1; j <= colCount; j++)
+                {
+                    //new line
+                    if (j == 1)
+                        Console.Write("\r\n");
+
+                    //write the value to the console
+                    if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                        Console.Write(xlRange.Cells[i, j].Value2.ToString() + "\t");
+
+                    //add useful things here!   
+                }
+            }
+
+
+
+            //cleanup
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            //rule of thumb for releasing com objects:
+            //  never use two dots, all COM objects must be referenced and released individually
+            //  ex: [somthing].[something].[something] is bad
+
+            //release com objects to fully kill excel process from running in the background
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            //close and release
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            //quit and release
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+
+        }
+
+        static void LerOLEDB()
+        {
+            string file = @"c:\tmp\arquivo.xls";
+
+            var dataSet = GetDataSetFromExcelFile(file);
+
+            int rowCount = dataSet.Tables[0].Rows.Count;
+            int colCount = dataSet.Tables[0].Columns.Count;
+
+            foreach (var item in dataSet.Tables[0].Select())
+            {
+                Console.WriteLine(item.Field<string>("NOME"));
+                Console.WriteLine(item.Field<string>("ENDEREÇO"));
+                Console.WriteLine(item.Field<string>("CPF"));
+
+            }
+
+        }
+
+        private static string GetConnectionString(string file)
+        {
+            Dictionary<string, string> props = new Dictionary<string, string>();
+
+            string extension = file.Split('.').Last();
+
+            if (extension == "xls")
+            {
+                //Excel 2003 and Older
+                props["Provider"] = "Microsoft.Jet.OLEDB.4.0";
+                props["Extended Properties"] = "Excel 8.0";
+            }
+            else if (extension == "xlsx")
+            {
+                //Excel 2007, 2010, 2012, 2013
+                props["Provider"] = "Microsoft.ACE.OLEDB.12.0;";
+                props["Extended Properties"] = "Excel 12.0 XML";
+            }
+            else
+                throw new Exception(string.Format("error file: {0}", file));
+
+            props["Data Source"] = file;
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (KeyValuePair<string, string> prop in props)
+            {
+                sb.Append(prop.Key);
+                sb.Append('=');
+                sb.Append(prop.Value);
+                sb.Append(';');
+            }
+
+            return sb.ToString();
+        }
+
+        private static DataSet GetDataSetFromExcelFile(string file)
+        {
+            DataSet ds = new DataSet();
+
+            string connectionString = GetConnectionString(file);
+
+            using (OleDbConnection conn = new OleDbConnection(connectionString))
+            {
+                conn.Open();
+                OleDbCommand cmd = new OleDbCommand();
+                cmd.Connection = conn;
+
+                // Get all Sheets in Excel File
+                DataTable dtSheet = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+                // Loop through all Sheets to get data
+                foreach (DataRow dr in dtSheet.Rows)
+                {
+                    string sheetName = dr["TABLE_NAME"].ToString();
+
+                    if (!sheetName.EndsWith("$"))
+                        continue;
+
+                    // Get all rows from the Sheet
+                    cmd.CommandText = "SELECT * FROM [" + sheetName + "]";
+
+                    DataTable dt = new DataTable();
+                    dt.TableName = sheetName;
+
+                    OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+                    da.Fill(dt);
+
+                    ds.Tables.Add(dt);
+                }
+
+                cmd = null;
+                conn.Close();
+            }
+
+            return ds;
+        }
+    }
+}
+
